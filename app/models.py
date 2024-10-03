060d2408fdf80511e498bf4379559bf37ba2890a
radman1007 <radman.cod110@gmail.com>
2024-10-03 16:53:38 +0330

    add a menu class

--- app/models.py
+++ app/models.py
@@ -13,35 +13,49 @@ class Reservation(models.Model):
     date = models.DateField()
     meal = models.CharField(max_length=11)
     number_of_people = models.CharField(max_length=20, choices=CHOICES_BOX)
-    
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Chef(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField()
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
-    
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
     message = models.TextField()
-    
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Gallery(models.Model):
     image = models.ImageField()
     title = models.CharField(max_length=100)
     text = models.TextField()
     number = models.CharField(max_length=20)
-    
+
     def __str__(self):
         return self.title
+
+
+class Menu(models.Model):
+    CHOICES_BOX = {
+        '1' : '1',
+        '2' : '2',
+        '3' : '3',
+        '4' : '4',
+    }
+    image = models.ImageField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    category = models.CharField(max_length=2, choices=CHOICES_BOX)
