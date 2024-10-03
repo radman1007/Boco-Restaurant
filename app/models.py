f24280ae1337f97a9e2a0e41f60da83098a27ed0
radman1007 <radman.cod110@gmail.com>
2024-10-03 14:04:50 +0330

    add a reservation class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Reservation(models.Model):
+    CHOICES_BOX = {
+        '1' : '1',
+        '2' : '2',
+        '3' : '3',
+        '4' : '4',
+        'ABOVE 4 UPTO 10' : 'Above 4 upto 10',
+    }
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+    date = models.DateField()
+    meal = models.CharField(max_length=11)
+    number_of_people = models.CharField(max_length=20, choices=CHOICES_BOX)
+    
