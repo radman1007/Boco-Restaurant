fa0785a70ae60d85fddd4dd4fe0e80a22df8fe3a
radman1007 <radman.cod110@gmail.com>
2024-10-03 15:23:09 +0330

    add a message in index def

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Gallery
 from .forms import ContactForm
+from django.contrib import messages
 
 def index(request):
     galleries = Gallery.objects.all()
@@ -8,7 +9,10 @@ def index(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your message has been successfully sent.')
             return redirect('index')
+        else:
+            messages.error(request, 'Please check the fields.')
     context = {
         'galleries' : galleries,
     }
