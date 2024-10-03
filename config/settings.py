0523dfffab6c590945414496505ea4719dc6499f
radman1007 <radman.cod110@gmail.com>
2024-10-03 13:25:15 +0330

    add a static and media file in base dir

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
