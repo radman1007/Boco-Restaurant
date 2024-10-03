fc0ac343f6fbbea2b76b0eaea87ea87316ff70bf
radman1007 <radman.cod110@gmail.com>
2024-10-03 14:17:24 +0330

    add a chef admin class

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Reservation, Chef
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'meal', 'number_of_people')
     ordering = ('-date',)
+
+
+@admin.register(Chef)
+class ChefAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
