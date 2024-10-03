452d890d28dc1e9cc302fa8545579786e85863b4
radman1007 <radman.cod110@gmail.com>
2024-10-03 15:06:38 +0330

    add a gitignore

--- app/models.py
+++ app/models.py
@@ -41,6 +41,7 @@ class Gallery(models.Model):
     image = models.ImageField()
     title = models.CharField(max_length=100)
     text = models.TextField()
+    number = models.CharField(max_length=20)
     
     def __str__(self):
         return self.title
--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Gallery
 
 def index(request):
-    return render(request, 'index.html')
+    galleries = Gallery.objects.all()
+    context = {
+        'galleries' : galleries
+    }
+    return render(request, 'index.html',  context)
